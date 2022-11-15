6f32e2375cb5fa6667258352c2a60e4d9970b0f3
mgmtsweni <mtswenigift@gmail.com>
2022-11-15 16:50:58 +0200

    code styele

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,6 +10,6 @@ def hello_world():
     """Function that displays Hello HBNB!"""
     return 'Hello HBNB!'
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     """Main Function """
     app.run(host='0.0.0.0', port=5000, debug=True)
