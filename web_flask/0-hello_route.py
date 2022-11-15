b19e859a316e780e90d9ed1c6416120f8920e1aa
mgmtsweni <mtswenigift@gmail.com>
2022-11-15 17:20:54 +0200

    Update style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
-
-
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-        def hello_world():
-        """Function that displays Hello HBNB!"""
-        return 'Hello HBNB!'
+def hello_world():
+    """Function that displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-    """Main Function """
+    """Main Function"""
     app.run(host='0.0.0.0', port=5000, debug=True)
