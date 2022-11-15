330cd75994750f650a7c4b79b266b91ecfe00745
mgmtsweni <mtswenigift@gmail.com>
2022-11-15 16:38:12 +0200

    code style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+  @app.route('/', strict_slashes=False)
 def hello_world():
-    """Function that displays Hello HBNB!"""
-    return 'Hello HBNB!!'
+        """Function that displays Hello HBNB!"""
+        return 'Hello HBNB!'
 
-if __name__=='__main__':
+    if __name__ =='__main__':
     """Main Function """
     app.run(host='0.0.0.0', port=5000, debug=True)
