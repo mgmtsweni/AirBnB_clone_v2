d7a79622440f8a9984b995726c8f9d9e4fd211e5
mgmtsweni <mtswenigift@gmail.com>
2022-11-16 17:21:11 +0200

    modify

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -29,13 +29,13 @@ def hello_python(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def isnum(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def isInterger(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -29,19 +29,19 @@ def hello_python(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def isnum(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def isInterger(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def isEven(n):
     """display a HTML page only if n is an integer"""
     e_o = "event" if (n % 2 == 0) else "odd"
