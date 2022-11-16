18f145342324a51014b7ebd57069780346614363
mgmtsweni <mtswenigift@gmail.com>
2022-11-16 16:48:36 +0200

    modify

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,8 +17,8 @@ def hbnb():
 
 
 @app.route('c/<text>', strict_slashes=False)
-def hello_c():
-    """display “C ” followed by the value of the text"""
+def croute(text):
+    """ display “C ”, followed by the value of the text variable """
     return 'C ' + text.replace('_', ' ')
 
 
